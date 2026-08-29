d7624ed4fa133ff340aa2f8efba01fadefe8dae9
乘风破浪 <72327160+mrdun@users.noreply.github.com>
2026-08-29 18:39:40 +0800

    fix(review): import GlobalModule to provide DB dependency

--- apps/api/src/review/review.module.ts
+++ apps/api/src/review/review.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 
+import { GlobalModule } from "../global/global.module";
 import { ReviewController } from "./review.controller";
 import { ReviewService } from "./review.service";
 
 @Module({
+  imports: [GlobalModule],
   providers: [ReviewService],
   controllers: [ReviewController],
 })
